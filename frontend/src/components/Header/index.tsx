411e937820f4b65aba44c45afb9e9ce65ae29fa8
Franklyn R <franklyn1998@live.com>
2022-07-13 12:06:33 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/img/logo.svg"
+
+import "./styles.css"
+
+const Header = () => {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="logo" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.instagram.com/franklyn_r.s/"> @franklyn_r.s</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
