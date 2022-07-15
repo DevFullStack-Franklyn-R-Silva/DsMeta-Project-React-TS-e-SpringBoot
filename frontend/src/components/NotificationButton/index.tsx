adaa0141efa060781cd904b8c96ab054b075c07b
Franklyn R <franklyn1998@live.com>
2022-07-15 11:58:12 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,16 +1,19 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 
 import "./styles.css";
 
-
 type Props = {
     saleId: number;
 }
+
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
-        .then((response) => console.log("SUCESSO"))
+        .then((response) => {
+            toast.info("SMS enviado com sucesso!")
+        })
 }
 
 
